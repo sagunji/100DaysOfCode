2cd70c5abffa9bc12a91d313407dcf7efe692be8
sagunji <karanjit.sagun01@gmail.com>
2019-07-16 12:13:10 +0545

    Remove unnecessary imports

--- python/5/gendates.py
+++ python/5/gendates.py
@@ -4,8 +4,6 @@ Every 100 days mark counting from PYBITES_BORN
 See the tests for more details how your code will be tested: as this is a beginner's challenge we only calculate a few years ahead, leaving the next leap year (2020) out of this challenge.
 We will revisit this in an intermediate challenge. Have fun!"""
 
-from itertools import islice
-
 from datetime import datetime, timedelta
 
 PYBITES_BORN = datetime(year=2016, month=12, day=19)
