a5c5903378f3068be3cf893a49c8ad231320c8a0
sagunji <karanjit.sagun01@gmail.com>
2019-07-17 21:31:22 +0545

    Day 7: Query a nested data structure

---
+++ python/7/cars.py
@@ -0,0 +1,37 @@
+cars = {
+    "Ford": ["Falcon", "Focus", "Festiva", "Fairlane"],
+    "Holden": ["Commodore", "Captiva", "Barina", "Trailblazer"],
+    "Nissan": ["Maxima", "Pulsar", "350Z", "Navara"],
+    "Honda": ["Civic", "Accord", "Odyssey", "Jazz"],
+    "Jeep": ["Grand Cherokee", "Cherokee", "Trailhawk", "Trackhawk"],
+}
+
+
+def get_all_jeeps(cars=cars):
+    """return a comma  + space (', ') separated string of jeep models
+       (original order)"""
+    return ", ".join(cars["Jeep"])
+
+
+def get_first_model_each_manufacturer(cars=cars):
+    """return a list of matching models (original ordering)"""
+    return [value[0] for value in cars.values()]
+
+
+def get_all_matching_models(cars=cars, grep="trail"):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    found_models = []
+    for key, value in cars.items():
+        for model in value:
+            if grep.lower() in model.lower():
+                found_models.append(model)
+    return found_models
+
+
+def sort_car_models(cars=cars):
+    """return a copy of the cars dict with the car models (values)
+       sorted alphabetically"""
+    return {key: sorted(value) for key, value in cars.items()}
+
